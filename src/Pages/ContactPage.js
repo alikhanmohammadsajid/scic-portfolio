9c74ce299b49d62a9ce66bf42d86a47eb024903c
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-05-02 21:53:26 +0600

    message box created

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -2,12 +2,36 @@ import { faMailBulk, faMapMarkedAlt, faPhone } from '@fortawesome/free-solid-svg
 import React from 'react';
 import ContactItem from '../Components/ContactItem';
 import Title from '../Components/Title';
+import emailjs from 'emailjs-com';
 
 const ContactPage = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_1py5m4c', 'template_ikyxnbl', e.target, 'user_s4raz1jUghitP1skA2fVr')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+        }
     return (
-        <div>
+        <div >
             <div className="title">
                 <Title title={"Contact"} span={"Contact"} />
+            </div>
+            <div>
+                <form className="contact-form" onSubmit={sendEmail}>
+                    <input type="hidden" name="contact_number" />
+                    <label>Name</label>
+                    <input type="text" name="name" />
+                    <label>Email</label>
+                    <input type="email" name="email" />
+                    <label>Message</label>
+                    <input type="text" name="message" />
+                    <input type="submit" value="Send" />
+                </form>
             </div>
             <div className="ContactPage">
 
