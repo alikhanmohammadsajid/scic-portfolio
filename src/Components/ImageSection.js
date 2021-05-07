494dff8b3c8bca674f6b2ad9ca4a91500b4f09a9
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-05-08 05:22:35 +0600

    resume updated

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -2,7 +2,7 @@ import { faFileAlt } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import myPicture from '../sk.png'
-import resume from '../resume/Sajid Khan Resume.pdf'
+import resume from '../resume/Sajids Resume.pdf'
 const ImageSection = () => {
     return (
         <div className="ImageSection">
