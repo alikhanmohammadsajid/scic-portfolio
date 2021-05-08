92977c3d0c009aca4876ea232ba52fd3635a2d7b
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-05-08 06:03:53 +0600

    project card added

---
+++ src/Components/Projectsdetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Projectsdetail = () => {
+    return (
+        <div ClassName="card mb-3" style="max-width: 540px;">
+            <div ClassName="row g-0">
+                <div ClassName="col-md-4">
+                    <img src="" alt=""/>
+    </div>
+                    <div ClassName="col-md-8">
+                        <div ClassName="card-body">
+                            <h5 ClassName="card-title">Card title</h5>
+                            <p ClassName="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p ClassName="card-text"><small ClassName="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default Projectsdetail;
